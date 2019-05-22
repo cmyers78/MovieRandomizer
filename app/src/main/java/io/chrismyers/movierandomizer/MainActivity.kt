cd2e733c5d74322f60ce99b5018531f56b1e8d7a
Chris Myers <chris.myers@rheem.com>
2019-05-22 17:51:46 -0400

    updates.

--- app/src/main/java/io/chrismyers/movierandomizer/MainActivity.kt
+++ app/src/main/java/io/chrismyers/movierandomizer/MainActivity.kt
@@ -2,11 +2,22 @@ package io.chrismyers.movierandomizer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val movieList = arrayListOf<String>("Captain Marvel", "Rio", "Rio 2", "The Last Jedi", "Charlie Brown", "Mary Poppins", "School of Rock")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomizeButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(movieList.count())
+            selectedMovie.text = randomFood.toString()
+        }
     }
 }
